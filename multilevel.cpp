a6d49d043cf1d0e23af6169f333096674c363416
mehwxsh <wish.meh03@gmail.com>
2024-08-06 23:56:07 +0530

    multilevel

---
+++ multilevel.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class vehicle{
+    public: 
+    int vno;
+    string color;
+
+    vehicle(int n = 0, string c = ""){
+
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
