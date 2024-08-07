5a99e5739e14d063db7b6f0e41ea104749399e26
mehwxsh <wish.meh03@gmail.com>
2024-08-07 22:51:57 +0530

    Multilevel Inheritance Vehicle-Scooter-Ola example

--- multilevel.cpp
+++ multilevel.cpp
@@ -1,18 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class vehicle{
-    public: 
+class vehicle
+{
+public:
     int vno;
     string color;
 
-    vehicle(int n = 0, string c = ""){
+    vehicle(int n = 0, string c = "") : vno(n), color(c) {}
 
+    void show()
+    {
+        cout << "Vehicle number: " << this->vno << endl
+             << "Color: " << this->color << endl;
+    }
+};
+
+class scooter : public vehicle
+{
+public:
+    string range;
+
+    scooter(int n = 0, string c = "", string m = "") : vehicle(n, c), range(m) {}
+
+    void show()
+    {
+        vehicle ::show();
+        cout << "Range: " << this->range << endl;
+    }
+};
+
+class ola : public scooter
+{
+public:
+    string chargingTime;
+
+    ola(int n = 0, string c = "", string m = "", string ct = "") : scooter(n, c, m), chargingTime(ct) {}
+
+    void show()
+    {
+        scooter ::show();
+        cout << "Charging Time: " << this->chargingTime << endl;
     }
 };
 
 int main()
 {
-    
+    ola ola1(123, "white", "120 Km", "6.5 Hr");
+    ola1.show();
+
     return 0;
 }
